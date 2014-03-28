ff2d863a3afbe3cd0d9c2cc030c344ce418b0038
fiatjaf <fiatjaf@gmail.com>
2014-03-28 17:04:12 -0300

    removed useless things, changed scope of the project
    * using coffeescript
    * separating the code in browser and node
    * wrote 'colors' and 'tables' utils
    * make myself the owner of everything

--- build/build.js
+++ build/build.js
@@ -6,58 +6,46 @@ var mime = require('mime');
 var seq = require('seq');
 
 // Do the build.
-var build = {};
 seq()
-	.seq(function () {
-		// Read in the project version.
-		fs.readFile('VERSION', 'ascii', this);
-	})
-	.seq(function(version) {
-		// Save the version number for later.
-		build.version = '' + version;
+  .seq(function () {
+    // Compile coffee-script
+    exec('./node_modules/.bin/coffee --compile --bare --output build/temp lib/browser.coffee')
+    exec('./node_modules/.bin/coffee --compile --bare --output build/temp lib/node.coffee')
+    exec('./node_modules/.bin/coffee --compile --bare --output build/temp lib/colors.coffee')
+    exec('./node_modules/.bin/coffee --compile --bare --output build/temp lib/tables.coffee')
 
-		// Copy raw JS, if required.
-		/*
-		fs.copyFileSync('lib/terminal.js', 'dist/terminal-' + build.version + '.js');
-		*/
+    // Browserify
+    exec('./node_modules/.bin/browserify build/temp/browser.js --standalone terminal --outfile dist/browser.js')
+    exec('./node_modules/.bin/browserify build/temp/node.js --standalone terminal --outfile dist/node.js')
+  })
+  .par(function () {
+    // Read in the less stylesheet.
+    fs.readFile('lib/terminal.less', 'ascii', this);
+  })
+  .par(function () {
+    // Read in 'interlace.png'.
+    fs.readFile('lib/interlace.png', this);
+  })
+  .par(function () {
+    // Read in 'external.png'.
+    fs.readFile('lib/external.png', this);
+  })
+  .seq(function(styles, interlacepng, externalpng) {
+    // Convert to string.
+    styles = '' + styles;
 
-		// Minify JS.
-		exec('java -jar build/closure/compiler.jar --compilation_level SIMPLE_OPTIMIZATIONS --js lib/terminal.js --js_output_file dist/terminal-' + build.version + '.min.js', this);
+    // Embed interlace.png
+    var interlace = 'url("data:' + mime.lookup('lib/interlace.png') + ';base64,' + new Buffer(interlacepng).toString('base64') + '")';
+    styles = styles.replace('dataurl("interlace.png")', interlace);
 
-		// Alternatively, use jsmin. It doesn't compress as much as the closure compiler.
-		/*
-		var jsmin = require('jsmin').jsmin;
-		var minified = jsmin(css, 3);
-		*/
-	})
-	.par(function () {
-		// Read in the less stylesheet.
-		fs.readFile('lib/terminal.less', 'ascii', this);
-	})
-	.par(function () {
-		// Read in 'interlace.png'.
-		fs.readFile('lib/interlace.png', this);
-	})
-	.par(function () {
-		// Read in 'external.png'.
-		fs.readFile('lib/external.png', this);
-	})
-	.seq(function(styles, interlacepng, externalpng) {
-		// Convert to string.
-		styles = '' + styles;
+    // Embed external.png
+    var external = 'url("data:' + mime.lookup('lib/external.png') + ';base64,' + new Buffer(externalpng).toString('base64') + '")';
+    styles = styles.replace('dataurl("external.png")', external);
 
-		// Embed interlace.png
-		var interlace = 'url("data:' + mime.lookup('lib/interlace.png') + ';base64,' + new Buffer(interlacepng).toString('base64') + '")';
-		styles = styles.replace('dataurl("interlace.png")', interlace);
-
-		// Embed external.png
-		var external = 'url("data:' + mime.lookup('lib/external.png') + ';base64,' + new Buffer(externalpng).toString('base64') + '")';
-		styles = styles.replace('dataurl("external.png")', external);
-
-		// Minify the stylesheet.
-		less.render(styles, {compress: true}, this);
-	})
-	.seq(function(css) {
-		// Write out the minified CSS.
-		fs.writeFile('dist/terminal-' + build.version + '.min.css', css);
-	});
+    // Minify the stylesheet.
+    less.render(styles, {compress: true}, this);
+  })
+  .seq(function(css) {
+    // Write out the minified CSS.
+    fs.writeFile('dist/terminal.min.css', css);
+  });
